ef7282363894c5d1620251c959174ad49bedd688
Alexander Bikadorov <bikaejkb@mailbox.tu-berlin.de>
2016-08-22 22:46:36 +0200

    Added status information parsing in interface

--- src/gitinterface.h
+++ src/gitinterface.h
@@ -16,6 +16,29 @@ struct BranchEntry {
     BranchEntry(QString n, QDateTime d, bool h) : name(n), date(d), isHead(h) {}
 };
 
+enum FileStatus {
+    STATUS_UNCHANGED = ' ',
+    STATUS_MODIFIED = 'M',
+    STATUS_ADDED = 'A',
+    STATUS_DELETED = 'D',
+    STATUS_RENAMED = 'R',
+    STATUS_COPIED = 'C',
+    STATUS_UPDATED = 'U',
+    STATUS_UNTRACKED = '?',
+    STATUS_IGNORED = '!'
+};
+
+struct StatusEntry {
+    QString name;
+    FileStatus statusIndex;
+    FileStatus statusTree;
+    QString renamedFrom;
+
+    StatusEntry() {} // needed by Q_DECLARE_METATYPE
+    StatusEntry(QString n, FileStatus x, FileStatus y, QString r = QString())
+        : name(n), statusIndex(x), statusTree(y), renamedFrom(r) {}
+};
+
 /**
  * @brief Communication interface between Git repo and application.
  */
@@ -27,24 +50,9 @@ class GitInterface : public QObject {
   public slots:
     void startUpdate(const QUrl &url) {
         m_lastUrl = url;
-        QString output =
-            run(argStart(url) << "for-each-ref"
-                              << "--shell"
-                              << "--format=%(HEAD) %(refname:short) %(authordate:iso8601)"
-                              << "refs/heads/");
 
-        const QStringList branches = output.split(QRegExp("[\r\n]"), QString::SkipEmptyParts);
-        // parse output; example line: "'*' 'master' '2016-02-11 16:09:38 +0100'"
-        QList<BranchEntry> branchList;
-        for (QString line : branches) {
-            QStringList values = line.split("'", QString::SkipEmptyParts);
-            bool isHead = values[0] == "*";
-            QString name = values[2];
-            QDateTime date = QDateTime::fromString(values[4], Qt::ISODate);
-            branchList.append(BranchEntry(name, date, isHead));
-        }
-
-        emit updatedBranches(branchList);
+        updateBranches(url);
+        updateStatus(url);
     }
 
     void checkout(const QString &branchName) {
@@ -62,13 +70,48 @@ class GitInterface : public QObject {
 
 signals:
   void updatedBranches(const QList<BranchEntry> &branches);
+  void updatedStatus(const QList<StatusEntry> &statuses);
   void repoChanged();
 
   private:
-    static QStringList argStart(const QUrl &url) {
-        QStringList arguments;
-        arguments << "-C" << url.toLocalFile();
-        return arguments;
+
+    void updateBranches(const QUrl &url) {
+        const QString output =
+            run(argStart(url) << "for-each-ref"
+                              << "--shell"
+                              << "--format=%(HEAD) %(refname:short) %(authordate:iso8601)"
+                              << "refs/heads/");
+
+        const QStringList branches = output.split(QRegExp("[\r\n]"), QString::SkipEmptyParts);
+        // parse output; example line: "'*' 'master' '2016-02-11 16:09:38 +0100'"
+        QList<BranchEntry> branchList;
+        for (const QString line : branches) {
+            const QStringList values = line.split("'", QString::SkipEmptyParts);
+            const bool isHead = values[0] == "*";
+            const QString name = values[2];
+            const QDateTime date = QDateTime::fromString(values[4], Qt::ISODate);
+            branchList.append(BranchEntry(name, date, isHead));
+        }
+
+        emit updatedBranches(branchList);
+    }
+
+    void updateStatus(const QUrl &url) {
+        const QString output = run(argStart(url) << "status"
+                                   << "-z");
+
+        const QStringList lines = output.split(QChar::Tabulation, QString::SkipEmptyParts);
+        QList<StatusEntry> statusList;
+        for (int i=0; i < lines.length(); i++) {
+            const QString line = lines[i];
+            const FileStatus statusIndex = static_cast<FileStatus>(line[0].toLatin1());
+            const FileStatus statusTree = static_cast<FileStatus>(line[1].toLatin1());
+            const QString file = line.mid(3);
+            statusList.append(StatusEntry(file, statusIndex, statusTree,
+                                          // if renamed next line is the file renamed file
+                                          statusIndex == STATUS_RENAMED ? lines[++i] : QString()));
+        }
+        emit updatedStatus(statusList);
     }
 
     QString run(const QStringList &arguments) {
@@ -88,12 +131,32 @@ signals:
                      << " exitCode=" << gitProcess->exitCode()
                      << " errOutput: " << gitProcess->readAllStandardError();
         }
-        return QString::fromUtf8(gitProcess->readAllStandardOutput().data());
+        QByteArray rawOutput = gitProcess->readAllStandardOutput();
+        // 'git status -z' divides by \0 character
+        return QString::fromUtf8(rawOutput.replace(QChar::Null, QChar::Tabulation));
+    }
+
+    static QStringList argStart(const QUrl &url) {
+        QStringList arguments;
+        arguments << "-C" << url.toLocalFile();
+        return arguments;
+    }
+
+    static FileStatus letterToStatus(const QChar &c) {
+        switch (c.toLatin1()) {
+            case ' ': return STATUS_UNCHANGED;
+            case 'A': return STATUS_ADDED;
+        }
+
+        return STATUS_IGNORED;
+        if (c == 'A') return STATUS_ADDED;
+        if (c == 'M') return STATUS_MODIFIED;
     }
 
     QUrl m_lastUrl;
 };
 
 Q_DECLARE_METATYPE(BranchEntry)
+Q_DECLARE_METATYPE(StatusEntry)
 
 #endif // GITINTERFACE_H
